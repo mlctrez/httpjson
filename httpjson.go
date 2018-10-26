bdedf9b5da8260874bcc527b41654cbf6b321647
mlctrez <mlctrez@gmail.com>
2018-10-25 21:31:52 -0500

    v1.0.0

--- httpjson.go
+++ httpjson.go
@@ -3,31 +3,72 @@ package httpjson
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"time"
 )
 
-type HttpClient struct {
+const ApplicationJson = "application/json"
+
+type HttpJson struct {
 	client *http.Client
 }
 
-func NewHttpClient(maxIdle int, requestTimeout int) *HttpClient {
+func New(maxIdle int, requestTimeout int) *HttpJson {
 	client := &http.Client{
 		Transport: &http.Transport{MaxIdleConnsPerHost: maxIdle},
 		Timeout:   time.Duration(requestTimeout) * time.Second,
 	}
-	return &HttpClient{client: client}
+	return NewWithClient(client)
 }
 
-func (c *HttpClient) PostJson(url string, request interface{}, response interface{}) error {
+func NewWithClient(client *http.Client) *HttpJson {
+	return &HttpJson{client: client}
+}
+
+func newRequest(method, url string, body io.Reader) (req *http.Request, err error) {
+	req, err = http.NewRequest(method, url, body)
+	if req != nil {
+		req.Header.Set("Accept", ApplicationJson)
+	}
+	return
+}
+
+func (c *HttpJson) Post(url string, request interface{}, response interface{}) error {
 
 	jsonBytes, err := json.Marshal(request)
 	if err != nil {
 		return err
 	}
 
-	resp, err := c.client.Post(url, "application/json", bytes.NewReader(jsonBytes))
+	req, err := newRequest(http.MethodPost, url, bytes.NewReader(jsonBytes))
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	respb, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return json.NewDecoder(bytes.NewReader(respb)).Decode(response)
+}
+
+func (c *HttpJson) Get(url string, response interface{}) error {
+
+	req, err := newRequest(http.MethodPost, url, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.client.Do(req)
 	if err != nil {
 		return err
 	}
